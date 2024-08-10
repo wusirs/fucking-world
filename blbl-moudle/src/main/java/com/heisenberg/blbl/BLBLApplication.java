119fb5063d63f678d7f06df4c542658838da09f0
Bella Ciao <2411851860@qq.com>
2024-08-10 11:37:16 +0800

    spring cloud init

--- blbl-moudle/src/main/java/com/heisenberg/blbl/BLBLApplication.java
+++ blbl-moudle/src/main/java/com/heisenberg/blbl/BLBLApplication.java
@@ -4,14 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * spring boot 启动类。
+ * @author heisenberg
+ * @date 2024/08/09
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class BLBLApplication {
     public static void main(String[] args) {
-        try {
-            SpringApplication.run(BLBLApplication.class, args);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        SpringApplication.run(BLBLApplication.class, args);
     }
 }
