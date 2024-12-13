9a574e54ba6de1a7788b80c352028a88c4a1e884
Bella Ciao <2411851860@qq.com>
2024-12-13 22:52:24 +0800

    share config

--- blbl-moudle/src/main/java/com/heisenberg/blbl/controller/ConfigController.java
+++ blbl-moudle/src/main/java/com/heisenberg/blbl/controller/ConfigController.java
@@ -1,6 +1,7 @@
 package com.heisenberg.blbl.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
@@ -8,13 +9,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/config")
 @RefreshScope
+@Slf4j
 public class ConfigController {
 
     @Value("${pattern.dateformat}")
     private String dateformat;
 
+    @Value("${psr.gateway.url:}")
+    private String psrUrl;
+
+    @Value("${psr.gateway.tUrl:}")
+    private String psrTUrl;
+
     @GetMapping("dateformat")
     public String now(){
+        log.info("url: {}, tUrl: {}", psrUrl, psrTUrl);
         return dateformat;
     }
 
