946754522257194df76c0df0b221fca2c0ac24dc
Bella Ciao <2411851860@qq.com>
2024-12-14 23:10:15 +0800

    项目结构整改

--- blbl-moudle/src/main/java/com/heisenberg/blbl/BLBLApplication.java
+++ blbl-moudle/src/main/java/com/heisenberg/blbl/BLBLApplication.java
@@ -3,6 +3,7 @@ package com.heisenberg.blbl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * spring boot 启动类。
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class BLBLApplication {
     public static void main(String[] args) {
         SpringApplication.run(BLBLApplication.class, args);
