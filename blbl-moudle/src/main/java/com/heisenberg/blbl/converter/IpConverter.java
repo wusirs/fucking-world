af87055e10c489ba076f3fa3386dc04922b549cd
Bella Ciao <2411851860@qq.com>
2024-12-15 12:31:20 +0800

    日志添加ip

---
+++ blbl-moudle/src/main/java/com/heisenberg/blbl/converter/IpConverter.java
@@ -0,0 +1,24 @@
+package com.heisenberg.blbl.converter;
+
+
+import ch.qos.logback.core.PropertyDefinerBase;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
+public class IpConverter extends PropertyDefinerBase {
+
+    @Override
+    public String getPropertyValue() {
+        String ip = "";
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.error("获取IP报错:", e);
+        }
+        return ip;
+    }
+}
+
