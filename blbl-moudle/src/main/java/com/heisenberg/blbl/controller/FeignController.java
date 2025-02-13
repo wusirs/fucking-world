f6617976e82024181d3f232767d785455cd96733
Bella Ciao <2411851860@qq.com>
2025-02-13 12:55:59 +0800

    feign 调用方式

--- blbl-moudle/src/main/java/com/heisenberg/blbl/controller/FeignController.java
+++ blbl-moudle/src/main/java/com/heisenberg/blbl/controller/FeignController.java
@@ -8,16 +8,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * feign 调用接口
+ * @author wl
+ */
 @RestController
 @RequestMapping("/feign")
 @Slf4j
 public class FeignController {
+    /**
+     * feign interface
+     */
     private final ConfigControllerFeign configControllerFeign;
 
     public FeignController(ConfigControllerFeign configControllerFeign) {
         this.configControllerFeign = configControllerFeign;
     }
 
+    /**
+     * 通过feign 调用接口
+     */
     @PostMapping("postFeign")
     public void postFeign(){
         ResponseEntity<String> res = configControllerFeign.dateformat();
--- blbl-moudle/src/main/java/com/heisenberg/blbl/feign/ConfigControllerFeign.java
+++ blbl-moudle/src/main/java/com/heisenberg/blbl/feign/ConfigControllerFeign.java
@@ -4,8 +4,17 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * feign 实现接口调用
+ * @author Ciao
+ */
 @FeignClient(value = "blbl-cloud", path = "/blbl-cloud")
 public interface ConfigControllerFeign {
+
+    /**
+     * feign 实现接口调用
+     * @return ResponseEntity<String>
+     */
     @GetMapping("/config/dateformat")
     ResponseEntity<String> dateformat();
 }
