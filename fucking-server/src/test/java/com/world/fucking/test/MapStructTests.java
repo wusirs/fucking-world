62effa1825ff8a72b546c6b59a09786377294597
Bella Ciao <2411851860@qq.com>
2025-03-12 18:29:35 +0800

    mapstruct 测试

---
+++ fucking-server/src/test/java/com/world/fucking/test/MapStructTests.java
@@ -0,0 +1,35 @@
+package com.world.fucking.test;
+
+import com.world.fucking.FuckingWorldApplication;
+import com.world.fucking.bean.po.BankCard;
+import com.world.fucking.bean.po.BankUser;
+import com.world.fucking.converter.BankUserConvert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@SpringBootTest(classes = FuckingWorldApplication.class)
+@RunWith(SpringRunner.class)
+public class MapStructTests {
+
+    @Autowired
+    private BankUserConvert bankUserConvert;
+
+    @Test
+    public void testMapStruct(){
+        System.out.println("_____________________________");
+        System.out.println(bankUserConvert);
+        List<BankCard> bankCardList = new ArrayList<>();
+        BankCard bankCard = new BankCard("123", BigDecimal.valueOf(12), 1, null);
+        bankCardList.add(bankCard);
+        BankUser bankUser = new BankUser("123", "张三", "12321421321", new Date(), "1,2,3", bankCardList);
+        System.out.println("AAAAAAAAA" +  bankUserConvert.po2Dto(bankUser));
+    }
+}
