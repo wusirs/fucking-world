3c5bc9e37043b6f67b828c5e8a5353ca322369f9
Bella Ciao <j2411851860@gmail.com>
2025-05-30 00:51:59 +0800

    style(format): 格式化代码

--- fucking-server/src/main/java/com/world/fucking/enums/ResultEnum.java
+++ fucking-server/src/main/java/com/world/fucking/enums/ResultEnum.java
@@ -1,22 +1,20 @@
 package com.world.fucking.enums;
 
 /**
- *
  * @author heisenberg
  * @since 1.0.0
  */
-public enum ResultEnum implements IResult{
+public enum ResultEnum implements IResult {
     SUCCESS(200, "成功"),
     VALIDATE_FAILED(400, "参数错误"),
     COMMON_FAILED(500, "系统错误"),
 
     FORBIDDEN(2004, "没有权限访问资源"),
-    TIME_EXCEPTION(3001, "时钟回拨，拒绝生成ID")
-    ;
-    private Integer code;
-    private String message;
+    TIME_EXCEPTION(3001, "时钟回拨，拒绝生成ID");
+    private final Integer code;
+    private final String message;
 
-    ResultEnum(Integer code, String message){
+    ResultEnum(Integer code, String message) {
         this.code = code;
         this.message = message;
     }
